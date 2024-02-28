2508795801682d0f2814a164b584954ad77e82d4
lawooze <153306128+lawooze@users.noreply.github.com>
2024-02-28 11:34:31 +0300

    Update Program.cs

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -13,24 +13,54 @@ namespace lab2
         }
     }
     public class Department
-{
-    public string Name { get; set; }
-    public string Location { get; set; }
-    public int EmployeeCount { get; set; }
-
-    public Department(string name, string location, int employeeCount)
     {
-        Name = name;
-        Location = location;
-        EmployeeCount = employeeCount;
-    }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int EmployeeCount { get; set; }
 
-    public void DisplayInformation()
+        public Department(string name, string location, int employeeCount)
+        {
+            Name = name;
+            Location = location;
+            EmployeeCount = employeeCount;
+        }
+
+        public void DisplayInformation()
+        {
+            Console.WriteLine("Подразделение: " + Name);
+            Console.WriteLine("Местоположение: " + Location);
+            Console.WriteLine("Количество сотрудников: " + EmployeeCount);
+        }
+}
+    class Занятие
     {
-        Console.WriteLine("Подразделение: " + Name);
-        Console.WriteLine("Местоположение: " + Location);
-        Console.WriteLine("Количество сотрудников: " + EmployeeCount);
-    }
+        private DateTime дата_проведения;
+        private string дисциплина;
+        private string сотрудник;
+        private string аудитория;
+        private string группа;
+        private string пара;
+        private string вид_занятия;
+
+        public Занятие(DateTime? дата_проведения = null, string дисциплина = "", string сотрудник = "", string аудитория = "", string группа = "", string пара = "", string вид_занятия = "")
+        {
+            if (дата_проведения == null)
+            {
+                this.дата_проведения = DateTime.Now;
+            }
+            else
+            {
+                this.дата_проведения = (DateTime)дата_проведения;
+            }
+
+            this.дисциплина = дисциплина;
+            this.сотрудник = сотрудник;
+            this.аудитория = аудитория;
+            this.группа = группа;
+            this.пара = пара;
+            this.вид_занятия = вид_занятия;
+            }
+
 }
 }
 
