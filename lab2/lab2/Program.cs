6ac5755047953ce6022eca9ada6911a8e4eb8eae
gachin05 <vgachin05@gmail.com>
2024-03-12 19:43:16 +0300

    добавлен класс DISCIPLINA вариант 3 выполнил Гачин Иван

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -59,8 +59,36 @@ namespace lab2
             this.группа = группа;
             this.пара = пара;
             this.вид_занятия = вид_занятия;
-            }
+        }
 
-}
+    }
+
+
+    /// <summary>
+    /// класс дисциплина
+    /// вариант 3
+    /// сделал Гачин Иван
+    /// </summary>
+    class DISCIPLINA
+    {
+        private string name;
+        private string sokrasch;
+
+        public DISCIPLINA(string name, string sokrasch)
+        {
+            this.name = name;
+            this.sokrasch = sokrasch;
+        }
+        public string Name()
+        {
+            return name;
+        }
+        public string Sokrasch()
+        {
+            return sokrasch;
+        }
+    }
+
+
 }
 
