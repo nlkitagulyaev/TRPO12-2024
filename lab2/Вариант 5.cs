874bb236fe2359e08e782254ffe7d747ef5185fb
nlkitagulyaev <161570202+nlkitagulyaev@users.noreply.github.com>
2024-03-12 23:14:20 +0300

    Add files via upload

---
+++ lab2/Вариант 5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_5variant
+{
+    class Student
+    {
+        private string Surname;
+
+        private string Name;
+
+        private string Patronymic;
+
+        private string Group;
+
+        private string DateOfBirthday;
+
+        public Student(string surname1, string name1, string patronymic1, string group1, string DateOfBirthday1)
+        {
+            Surname = surname1;
+            Name = name1;
+            Patronymic = patronymic1;
+            Group = group1;
+            DateOfBirthday = DateOfBirthday1;
+        }
+    }
+}
