f7b93c4ff0831e2820c84e55b731cf91ddbd1f0a
Dyce-a <fiaskotarasov@gmail.com>
2024-03-12 19:49:32 +0300

    Добавил класс Organizaciya Вариант 12 Тарасов Илья

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -88,6 +88,26 @@ namespace lab2
             return sokrasch;
         }
     }
+    /// <summary>
+    /// Класс организация
+    /// Вариант 12
+    /// Тарасов Илья
+    /// </summary>
+    public class Organizaciya
+    {
+        private string _nazvanie;
+        private string _yuridicheskiyAdres;
+        private string _fakticheskiyAdres;
+        //private Sotrudnik _rukovoditel; //класс сотрудник еще не создан
+
+        public Organizaciya(string nazvanie, string yuridicheskiyAdres, string fakticheskiyAdres /*Sotrudnik rukovoditel*/)// класс пока еще не создан
+        {
+            _nazvanie = nazvanie;
+            _yuridicheskiyAdres = yuridicheskiyAdres;
+            _fakticheskiyAdres = fakticheskiyAdres;
+            //_rukovoditel = rukovoditel;
+        }
+    }
 
 
 }
