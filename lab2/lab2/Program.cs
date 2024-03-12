82f8f5d485ab4c8a812a916480b60229184f409d
expika <expika@EXPIKA>
2024-03-12 20:51:26 +0300

    Добавил класс Group Вариант 4 Голубов Фёдор

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -109,6 +109,31 @@ namespace lab2
         }
     }
 
+    public class Group
+    {
+        private string name;
+        private string sokrasch;
+        private int amount;
+        private int year;
+        private string link;
+        private string boss;
 
+        public Group(string name, string sokrasch, int amount, int year, string link, string boss)
+        {
+            this.name = name;
+            this.sokrasch = sokrasch;
+            this.amount = amount;
+            if (year == null)
+            {
+                this.year = year;
+            }
+            else
+            {
+                this.year = DateTime.Now.Year;
+            }
+            this.link = link;
+            this.boss = boss;
+        }
+    }
 }
 
