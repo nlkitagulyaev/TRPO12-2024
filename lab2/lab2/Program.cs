d54d4765042790ac30171ee87afd6f9ee86d28fd
INeksiI <keroninch201@gmail.com>
2024-03-13 10:20:16 +0300

    Добавлен класс Должность, а также основа для класса подразделение Вариант 10 Ли Михаил

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -89,6 +89,51 @@ namespace lab2
         }
     }
     /// <summary>
+    /// Класс Должность
+    /// Вариант 10
+    /// Сделал Ли Михаил
+    /// </summary>
+    public class Dolznost
+    {
+        private string nazvanie;
+        private double oklad;
+        private Podrazdelenie podrozdelenie;
+
+        public Dolznost(string nazvanie, double oklad, Podrazdelenie podrazdelenie)
+        {
+            this.nazvanie = nazvanie;
+            this.oklad = oklad;
+            this.podrozdelenie = podrazdelenie;
+        }
+
+        public string Nazvanie
+        {
+            get { return nazvanie; }
+            set { nazvanie = value; }
+        }
+
+        public double Oklad
+        {
+            get { return oklad; }
+            set { oklad = value; }
+        }
+    }
+
+    /// <summary>
+    /// Класс Подразделение
+    /// Вариант 11
+    /// Сделал ...
+    /// </summary>
+    public class Podrazdelenie
+    {
+        private string nazvanie;
+        public string Nazvanie
+        {
+            get { return nazvanie; }
+            set { nazvanie = value; }
+        }
+    }
+    /// <summary>
     /// Класс организация
     /// Вариант 12
     /// Тарасов Илья
